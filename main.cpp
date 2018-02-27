78874ae96d9986e50eeed1cc3c6388b19a0fb683
Vitalka555 <vsantipov@mail.ru>
2018-02-27 20:15:48 +0300

    Исправлен баг с копированием изображений в папки для фото и схем. Ранее копировалось в корневые с exe, теперь путь к папкам берем из settings.ini

--- main.cpp
+++ main.cpp
@@ -34,12 +34,12 @@ static const int PROGRESS_HEIGHT_PX = 28;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
+    //QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
     //QLocale::setDefault(QLocale::C);
     //QLocale::setDefault(QLocale::C);
     //setlocale(LC_NUMERIC, "ru_RU.UTF-8");
 #if defined(Q_OS_WIN)
-    //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
     //QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
